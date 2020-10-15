73d7364f1527846c98217a80a238067f456deeae
adrian090807 <47449403+adrian090807@users.noreply.github.com>
2020-10-15 10:10:41 +0300

    Add files via upload

---
+++ im2pdf.py
@@ -0,0 +1,25 @@
+import sys
+import tkinter as tk
+import subprocess
+from tkinter import filedialog
+from PIL import Image
+import os
+
+root = tk.Tk()
+root.withdraw()
+root.call('wm', 'attributes', '.', '-topmost', True)
+files = filedialog.askopenfilename(multiple=True) 
+var = root.tk.splitlist(files)
+images=[]
+for i in files:
+    images.append(Image.open(i))
+
+im1=images.pop(0)
+dir = filedialog.askdirectory()
+
+filename = "cumulat.pdf"
+filepath = os.path.join(dir,filename)
+
+
+
+im1.save(filepath, "PDF" ,resolution=100.0, save_all=True, append_images=images)
